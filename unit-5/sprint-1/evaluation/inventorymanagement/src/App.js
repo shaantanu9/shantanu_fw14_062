2b41c63914007b4c6ead8c918b8b58f7e7241361
shantanu <shantanubombatkar2@gmail.com>
2022-03-07 09:47:59 +0530

    9.47am

---
+++ unit-5/sprint-1/evaluation/inventorymanagement/src/App.js
@@ -0,0 +1,17 @@
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Body } from './components/body';
+function App() {
+  return (
+    <div className='App'>
+    <h1 className='text-3xl underline animate-bounce hover:bg-red-500'>
+      Hello world!
+    </h1>
+{/* <Header /> 
+<Body /> 
+<Footer />  */}
+    </div>
+  );
+}
+
+export default App;
