e8491b1610b792beff7da7b32a12a1cffa7711fd
shantanu <shantanubombatkar2@gmail.com>
2022-03-14 09:49:23 +0530

    check

--- unit-5/sprint-2/evaluation/rent/src/App.jsx
+++ unit-5/sprint-2/evaluation/rent/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
 
@@ -7,8 +7,11 @@ function App() {
     <div className="App">
       <button className="toggleForm">
         {/* Show text Add House or Show Rentals based on state */}
+
       </button>
       {/* Show component based on state */}
+
+    <button className='px-1 m-1 rounded  border-2 border-blue-500 hover:bg-white hover:text-blue-500 text-white bg-blue-500'>Button</button>
       <br />
     </div>
   );
